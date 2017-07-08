00a28df4055123cfbe457bbe628365ca5e5574db
Johan Mabille <johan.mabille@gmail.com>
2017-07-08 02:00:59 +0200

    disabling -march=native options on Linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# cryptopp files are stored with CRLF eol, making impossible to patch them the conda way
+# can be remover in the enxt release of cryptopp since its cmakelists will have an option
+# to remove march=generic flag
+#sed -i s/$/$'\r'/ $RECIPE_DIR/0001-cmake-mtune-fix.patch
+#git apply --binary $RECIPE_DIR/0001-cmake-mtune-fix.patch
 mkdir build
 cd build
-cmake -D BUILD_SHARED=OFF -D BUILD_TESTING=OFF -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_LIBDIR=lib -D CMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake -D BUILD_SHARED=OFF -D DISABLE_SSSE3=ON -D BUILD_TESTING=OFF -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_LIBDIR=lib -D CMAKE_INSTALL_PREFIX=$PREFIX ..
 make
 make install
