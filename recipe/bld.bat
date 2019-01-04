39f23194d1c2dab9f54525909ee1c1aed6a2ca2f
Sylvain Corlay <sylvain.corlay@gmail.com>
2019-01-04 17:40:19 +0100

    Do not build cryptopp.dll on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,7 +10,7 @@ COPY cryptopp-cmake\CMakeLists.txt
 :: Regular cmake build
 mkdir build
 cd build
-cmake -G "NMake Makefiles" -D DISABLE_SSSE3=ON -D BUILD_TESTING=OFF -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
+cmake -G "NMake Makefiles" -D DISABLE_SSSE3=ON -D BUILD_SHARED=OFF -D BUILD_TESTING=OFF -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
 if errorlevel 1 exit 1
 nmake
 if errorlevel 1 exit 1
