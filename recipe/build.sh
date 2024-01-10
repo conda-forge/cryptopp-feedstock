60cf48df8c49b49be117334003cd6e6a5fb375c2
Austin Morton <apmorton@users.noreply.github.com>
2024-01-10 13:00:30 -0600

    MNT: Re-rendered with conda-build 3.24.0, conda-smithy 3.30.1, and conda-forge-pinning 2023.12.12.14.02.26 (#31)

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,9 +9,20 @@ cd ..
 cp "$PWD/cryptopp-cmake/cryptopp-config.cmake" "$PWD"
 cp "$PWD/cryptopp-cmake/CMakeLists.txt" "$PWD"
 
+if [[ "${target_platform}" == osx-* ]]; then
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 # Regular cmake build
 mkdir build
 cd build
-cmake -D DISABLE_SSSE3=ON -D BUILD_TESTING=OFF -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_LIBDIR=lib -D CMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake \
+  -D DISABLE_SSSE3=ON \
+  -D BUILD_TESTING=OFF \
+  -D CMAKE_BUILD_TYPE=Release \
+  -D CMAKE_INSTALL_LIBDIR=lib \
+  -D CMAKE_INSTALL_PREFIX=$PREFIX \
+  ..
 make
 make install
