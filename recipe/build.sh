49f2aab29667452a78396549ed2f5235944eca93
Austin Morton <austin.morton@aquatic.com>
2024-01-10 18:38:36 +0000

    Try again

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,11 @@ cd ..
 cp "$PWD/cryptopp-cmake/cryptopp-config.cmake" "$PWD"
 cp "$PWD/cryptopp-cmake/CMakeLists.txt" "$PWD"
 
+if [[ "${target_platform}" == osx-* ]]; then
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 # Regular cmake build
 mkdir build
 cd build
@@ -18,7 +23,6 @@ cmake \
   -D CMAKE_BUILD_TYPE=Release \
   -D CMAKE_INSTALL_LIBDIR=lib \
   -D CMAKE_INSTALL_PREFIX=$PREFIX \
-  -D _LIBCPP_DISABLE_AVAILABILITY \
   ..
 make
 make install
