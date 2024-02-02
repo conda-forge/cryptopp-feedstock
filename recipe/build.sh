62a449dfe710d5b6a5bbf73288a6d725398000ae
Austin Morton <austin.morton@aquatic.com>
2024-02-02 02:55:05 +0000

    cryptopp v8.6.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Adding the cmake support for cryptopp, which dropped cmake in 6.0.
-git clone https://github.com/noloader/cryptopp-cmake.git
-cd cryptopp-cmake
-git checkout CRYPTOPP_7_0_0
-cd ..
-
 cp "$PWD/cryptopp-cmake/cryptopp-config.cmake" "$PWD"
 cp "$PWD/cryptopp-cmake/CMakeLists.txt" "$PWD"
 
@@ -17,12 +11,10 @@ fi
 # Regular cmake build
 mkdir build
 cd build
-cmake \
-  -D DISABLE_SSSE3=ON \
+cmake ${CMAKE_ARGS} \
   -D BUILD_TESTING=OFF \
   -D CMAKE_BUILD_TYPE=Release \
   -D CMAKE_INSTALL_LIBDIR=lib \
-  -D CMAKE_INSTALL_PREFIX=$PREFIX \
   ..
-make
+make -j $CPU_COUNT
 make install
