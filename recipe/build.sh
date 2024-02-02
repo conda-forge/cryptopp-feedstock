be1381e443d40ec81430e7753ff0dbe81e7e4205
Austin Morton <apmorton@users.noreply.github.com>
2024-02-02 01:56:34 -0500

    cryptopp v8.9.0 (#35)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-cp "$PWD/cryptopp-cmake/cryptopp-config.cmake" "$PWD"
-cp "$PWD/cryptopp-cmake/CMakeLists.txt" "$PWD"
-
 if [[ "${target_platform}" == osx-* ]]; then
     # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
     CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
 # Regular cmake build
-mkdir build
-cd build
 cmake ${CMAKE_ARGS} \
-  -D BUILD_TESTING=OFF \
   -D CMAKE_BUILD_TYPE=Release \
   -D CMAKE_INSTALL_LIBDIR=lib \
-  ..
-make -j $CPU_COUNT
-make install
+  -D CRYPTOPP_BUILD_TESTING=OFF \
+  -D CRYPTOPP_SOURCES=$PWD \
+  -S cryptopp-cmake \
+  -B build
+make -C build -j $CPU_COUNT
+make -C build install
